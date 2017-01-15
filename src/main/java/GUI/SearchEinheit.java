b5d7720bb8724b41f5b91473e4bf3384cc389e63
Whoop-SWT <whoopschmidt@googlemail.com>
2017-01-15 14:25:23 +0100

    Neue Version, beware of changed names etc

--- src/main/java/GUI/SearchEinheit.java
+++ src/main/java/GUI/SearchEinheit.java
@@ -56,42 +56,41 @@ public class SearchEinheit implements ActionListener {
     private void initialize() {
         frame = new JFrame();
         frame.setBounds(100, 100, 470, 420);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JLabel label = new JLabel("Suchen eines Termins");
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        label.setFont(new Font("Arial", Font.PLAIN, 20));
+        JLabel lblSuchenEinerEinheit = new JLabel("Suchen einer Einheit");
+        lblSuchenEinerEinheit.setHorizontalAlignment(SwingConstants.CENTER);
+        lblSuchenEinerEinheit.setFont(new Font("Arial", Font.PLAIN, 20));
 
-        txtFieldInput = new JFormattedTextField();
-        txtFieldInput.setToolTipText("Wonach soll gesucht werden?");
-        txtFieldInput.setFont(new Font("Arial", Font.PLAIN, 11));
+        JFormattedTextField ftfSuchbegriff = new JFormattedTextField();
+        ftfSuchbegriff.setToolTipText("Wonach soll gesucht werden?");
+        ftfSuchbegriff.setFont(new Font("Arial", Font.PLAIN, 11));
 
-        JLabel label_1 = new JLabel("Suchbegriff:");
-        label_1.setFont(new Font("Arial", Font.PLAIN, 11));
+        JLabel lbSuchbegriff = new JLabel("Suchbegriff:");
+        lbSuchbegriff.setFont(new Font("Arial", Font.PLAIN, 11));
 
-        JLabel label_2 = new JLabel("Attribut:");
-        label_2.setFont(new Font("Arial", Font.PLAIN, 11));
+        JLabel lbAttribut = new JLabel("Attribut:");
+        lbAttribut.setFont(new Font("Arial", Font.PLAIN, 11));
 
-        comboBox = new JComboBox();
-        comboBox.setModel(new DefaultComboBoxModel(new String[] {"Name der Veranstaltung", "Datum", "Dozent", "Priorit\u00E4t", "Raum", "Startzeit"}));
-        comboBox.setToolTipText("Bitte Attribut ausw\u00E4hlen z.B. Ort, Dozent etc.");
-        comboBox.setFont(new Font("Arial", Font.PLAIN, 11));
+        JComboBox boxAttribut = new JComboBox();
+        boxAttribut.setModel(new DefaultComboBoxModel(new String[]{"Name der Veranstaltung", "Datum", "Dozent", "Priorit\u00E4t", "Raum", "Uhrzeit"}));
+        boxAttribut.setToolTipText("Bitte Attribut ausw\u00E4hlen z.B. Ort, Dozent etc.");
+        boxAttribut.setFont(new Font("Arial", Font.PLAIN, 11));
 
         JLabel lblErgebnis = new JLabel("Ergebnis:");
 
         lblErgebnis.setFont(new Font("Arial", Font.PLAIN, 11));
 
-        JButton btnsucheAusfhren = new JButton("Suchen\r\n");
-        btnsucheAusfhren.setToolTipText("Klicken um Suche zu starten");
-        btnsucheAusfhren.setFont(new Font("Arial", Font.PLAIN, 11));
-        btnsucheAusfhren.addActionListener(this);
+        JButton btnSuchen = new JButton("Suchen\r\n");
+        btnSuchen.setToolTipText("Klicken um Suche zu starten");
+        btnSuchen.setFont(new Font("Arial", Font.PLAIN, 11));
 
-        JFormattedTextField formattedTextField_1 = new JFormattedTextField();
-        formattedTextField_1.setToolTipText("Kontrollanzeige");
+        JFormattedTextField KontrollAnzeige = new JFormattedTextField();
+        KontrollAnzeige.setEditable(false);
+        KontrollAnzeige.setToolTipText("Kontrollanzeige");
 
         JList lsErgebnis = new JList();
         lsErgebnis.setFont(new Font("Arial", Font.PLAIN, 11));
-
 
 
         GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
@@ -102,46 +101,46 @@ public class SearchEinheit implements ActionListener {
                                 .addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
                                         .addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
                                                 .addGroup(groupLayout.createSequentialGroup()
-                                                        .addComponent(formattedTextField_1, GroupLayout.DEFAULT_SIZE, 434, Short.MAX_VALUE)
+                                                        .addComponent(KontrollAnzeige, GroupLayout.DEFAULT_SIZE, 434, Short.MAX_VALUE)
                                                         .addContainerGap())
                                                 .addGroup(groupLayout.createSequentialGroup()
                                                         .addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
                                                                 .addComponent(lblErgebnis)
-                                                                .addComponent(label_2, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
-                                                                .addComponent(label_1, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE))
+                                                                .addComponent(lbAttribut, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
+                                                                .addComponent(lbSuchbegriff, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE))
                                                         .addPreferredGap(ComponentPlacement.RELATED)
                                                         .addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
                                                                 .addComponent(lsErgebnis, GroupLayout.PREFERRED_SIZE, 256, GroupLayout.PREFERRED_SIZE)
-                                                                .addComponent(label, GroupLayout.PREFERRED_SIZE, 271, GroupLayout.PREFERRED_SIZE)
+                                                                .addComponent(lblSuchenEinerEinheit, GroupLayout.PREFERRED_SIZE, 271, GroupLayout.PREFERRED_SIZE)
                                                                 .addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
-                                                                        .addComponent(txtFieldInput, Alignment.LEADING)
-                                                                        .addComponent(comboBox, Alignment.LEADING, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                                                                        .addComponent(ftfSuchbegriff, Alignment.LEADING)
+                                                                        .addComponent(boxAttribut, Alignment.LEADING, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                                                         .addContainerGap(109, Short.MAX_VALUE)))
                                         .addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-                                                .addComponent(btnsucheAusfhren, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE)
+                                                .addComponent(btnSuchen, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE)
                                                 .addContainerGap())))
         );
         groupLayout.setVerticalGroup(
                 groupLayout.createParallelGroup(Alignment.TRAILING)
                         .addGroup(groupLayout.createSequentialGroup()
                                 .addContainerGap()
-                                .addComponent(label, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(lblSuchenEinerEinheit, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
                                 .addGap(44)
                                 .addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-                                        .addComponent(label_1)
-                                        .addComponent(txtFieldInput, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(lbSuchbegriff)
+                                        .addComponent(ftfSuchbegriff, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
                                 .addPreferredGap(ComponentPlacement.UNRELATED)
                                 .addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-                                        .addComponent(label_2)
-                                        .addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(lbAttribut)
+                                        .addComponent(boxAttribut, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
                                 .addPreferredGap(ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
                                 .addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
                                         .addComponent(lsErgebnis, GroupLayout.PREFERRED_SIZE, 142, GroupLayout.PREFERRED_SIZE)
                                         .addComponent(lblErgebnis))
                                 .addGap(12)
-                                .addComponent(btnsucheAusfhren, GroupLayout.PREFERRED_SIZE, 50, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(btnSuchen, GroupLayout.PREFERRED_SIZE, 50, GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(ComponentPlacement.RELATED)
-                                .addComponent(formattedTextField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                                .addComponent(KontrollAnzeige, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
         );
         frame.getContentPane().setLayout(groupLayout);
     }
